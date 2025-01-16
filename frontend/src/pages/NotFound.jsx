998b37ff00612dff1289fa8e689af57ee018034d
bithulmb <bithulmb@gmail.com>
2025-01-16 16:09:40 +0530

    initial commit

---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div>
+     <h1> Not found</h1>
+     <h4>The page you are looking could not be found </h4>
+    </div>
+  )
+}
+
+export default NotFound
